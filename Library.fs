71c6e989ab88cf71b5c4a7caf3222e4ad9c52b96
Nick Fisher <nick.fisher@avinium.com>
2019-09-15 16:59:32 +1000

    use L2 loss fn

--- Library.fs
+++ Library.fs
@@ -7,39 +7,43 @@ open MathNet.Numerics
 open MathNet.Numerics.LinearAlgebra
 open MathNet.Numerics.Distributions;
 open MathNet.Numerics.Random;
+open MathNet.Numerics.LinearAlgebra.Factorization
 
 module FeedbackAlignment =
  
   type NN (dim:int) = 
-    
-    let mutable w1 = Matrix<double>.Build.Random(dim, 2); // weights
+    // weights, orthogonal initialization     let ortho = Matrix<double>.Build.Random(dim * 2, dim + 2).QR().Q;
+    let mutable w1 = Matrix<double>.Build.Random(dim, 2); // ortho.SubMatrix(0, dim, 0, 2) // 
     let mutable z1 = Matrix<double>.Build.Random(dim, 1); // first layer activation input
     let mutable w1' = Matrix<double>.Build.Random(dim, 2); // first layer grads
-    let mutable w2 = Matrix<double>.Build.Random(dim, dim); // second layer weights
+    let mutable w2 = Matrix<double>.Build.Random(dim, dim)// ortho.SubMatrix(dim, dim, 2, dim);//Matrix<double>.Build.Random(dim, dim).QR().Q.Transpose(); // second layer weights
     let mutable w2' = Matrix<double>.Build.Random(dim, dim); // second layer grads 
     let mutable z2 = Matrix<double>.Build.Random(dim, 1); // second layer activation input
     let mutable w3 = Matrix<double>.Build.Random(1, dim); // linear output weights
     let mutable err = 0.0
     let relu x = max x 0.0 
     let relu' x = match x > 0.0 with | true -> 1.0 | false -> 0.0 
+    let r2 = Matrix<double>.Build.Random(1, 10)
+    let r1 = Matrix<double>.Build.Random(dim, dim)
 
     member x.Forward (input:Matrix<double>) (output:float) = 
-      z1 <- Matrix.map relu (w1 * input) // (dx2) * (2x1) -> (dx1)
-      z2 <- Matrix.map relu (w2 * z1)
+      z1 <- w1 * input |> Matrix.map relu // (dx2) * (2x1) -> (dx1)
+      z2 <- w2 * z1 |> Matrix.map relu
       let y_hat = (w3 * z2).Row(0).Item(0)
       if y_hat.Equals(nan) then
         failwith "NAN"
-      err <- match y_hat > output with | true -> 1.0 | false -> -1.0
-      printfn "Loss %f" (abs(y_hat - output))
+      let loss = (y_hat - output) ** 2.0
+      err <- (y_hat - output)
       y_hat
+    
     
     member x.Backward (input:Matrix<double>) (symmetric:bool) =
       if symmetric then 
         w2' <- (err * w3.Transpose()).PointwiseMultiply(Matrix.map relu' z2) 
         w1' <- (w2.Transpose() * w2').PointwiseMultiply((Matrix.map relu' z1)) 
-      // else     
-      //   w2' <- Matrix<double>.Build.Random(3, 1) * w3' * (Matrix.map relu' z2)  // (3 x 1) * (1 x 3)
-      //   w1' <- Matrix<double>.Build.Random(3, 1) * w2' * (Matrix.map relu' z1) // (3 x 1) * (1 x 3)
+      else     
+        w2' <- (err * r2.Transpose()).PointwiseMultiply(Matrix.map relu' z2) 
+        w1' <- (r1 * w2').PointwiseMultiply((Matrix.map relu' z1)) 
 
     member x.Update (lr:double) (inputs:Matrix<double>)= 
       w1 <- w1 - (lr * w1' * inputs.Transpose())
@@ -51,12 +55,13 @@ module FeedbackAlignment =
   let inputs = [ 0.0,1.0,1.0; 1.0,0.0,1.0; 1.0,1.0,0.0; 0.0,0.0,0.0 ];
   let next () = List.item (rnd.Next inputs.Length) inputs
   let mutable accuracies:float list = []
-  for i in seq { 0..10000 } do 
+  let iterations = 5000
+  for i in seq { 0..iterations } do 
     let (x1,x2,y1) = next()
     let x = array2D [ [x1; ]; [x2] ]  |> DenseMatrix.ofArray2  
     nn.Forward x y1 |> ignore
-    nn.Backward x true
-    nn.Update 0.0001 x
+    nn.Backward x false
+    nn.Update 0.001 x
     if i % 100 = 0 then
       let preds = seq {
         for j in seq { 0..20 } do
